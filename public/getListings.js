45e84d1d365481d8d27f73b7c25b7071ec775374
VK4041 <151887513+VK4041@users.noreply.github.com>
2024-05-27 00:37:21 +1000

    Add collapse element to each card

--- public/getListings.js
+++ public/getListings.js
@@ -11,50 +11,57 @@ function addCards(cards) {
 
         //Raw HTML content for card
         let div = document.createElement('div');
-        div.classList.add("col-lg-3","col-md-4","col-sm-6", "my-2");
-        div.innerHTML = `
-                <div class="card">
-                    <img src= "${card.path}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Property Details</h5>
-                        <p class="card-text lead">Owner Name: ${card.oname}</p>
-                        <p class="card-text lead">Owner Age: ${card.oage}</p>
-                        <p class="card-text lead">Owner Contact: ${card.ono}</p>
-                        <p class="card-text lead">Price (AUD): ${card.price}</p>
+        div.classList.add("col-lg-3", "col-md-4", "col-sm-6", "my-2");
+        // div.innerHTML = `
+        //         <div class="card">
+        //             <img src= "${card.path}" class="card-img-top">
+        //             <div class="card-body">
+        //                 <h5 class="card-title text-center">Property Details</h5>
+        //                 <p class="card-text lead">Owner Name: ${card.oname}</p>
+        //                 <p class="card-text lead">Owner Age: ${card.oage}</p>
+        //                 <p class="card-text lead">Owner Contact: ${card.ono}</p>
+        //                 <p class="card-text lead">Price (AUD): ${card.price}</p>
+        //                 <p class="card-text lead">House No: ${card.hno}</p>
+        //                 <p class="card-text lead">Street: ${card.street}</p>
+        //                 <p class="card-text lead">Suburb: ${card.suburb}</p>
+        //                 <p class="card-text lead">State: ${card.state}</p>
+        //                 <p class="card-text lead">Area Code: ${card.acode}</p>
+        //                 <p class="card-text lead">Description: ${card.description}</p>
+
+        //             </div>
+        //         </div>
+        // `;
+        div.innerHTML = `<div class="card">
+                <img src= "${card.path}" class="card-img-top">
+                <div class="card-body">
+                    <p class="card-text lead fst-italic">${card.suburb}, ${card.state}</p>
+                    <p class="card-text fs-4 fw-bold">Price (AUD): ${card.price}/-</p>
+                    <p class="card-text lead">"${card.description}"</p>
+                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#listing${idCount}">
+                        Show Details
+                    </button>
+                    <div class="collapse" id="listing${idCount}">
+                    <div class="card card-body details">
+                        <p class="card-text lead">Posted by: ${card.oname} (${card.oage})</p>
+                        <p class="card-text lead">Contact: ${card.ono}</p>
+                        <p class="card-text lead fs-3">Address</p>
                         <p class="card-text lead">House No: ${card.hno}</p>
                         <p class="card-text lead">Street: ${card.street}</p>
                         <p class="card-text lead">Suburb: ${card.suburb}</p>
                         <p class="card-text lead">State: ${card.state}</p>
                         <p class="card-text lead">Area Code: ${card.acode}</p>
-                        <p class="card-text lead">Description: ${card.description}</p>
-                        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#listing${idCount}">
-                            Click Me for Details
-                        </button>
+                    </div>
                     </div>
                 </div>
+            </div>
         `;
         cardRow.appendChild(div);
-        offCanvas(idCount++, card.oname, card.oage, card.description);
+        idCount++;
+        // cardRow.appendChild(div);
     });
 }
 
 container.appendChild(cardRow);
-
-function offCanvas(ID, oname, oage, description) {
-    let offcanvas = document.createElement('div');
-    offcanvas.innerHTML = `
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="listing${ID}">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title">Listed by: ${oname} (${oage})</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
-            </div>
-            <div class="offcanvas-body">
-                ${description}
-            </div>
-        </div>
-    `;
-    container.appendChild(offcanvas);
-}
 
 body.appendChild(container);
 const getCards = () => {
